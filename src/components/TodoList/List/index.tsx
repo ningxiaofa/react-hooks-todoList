913ac52263073d3414716beb97b4869d773aa59d
williamning <61674789+williamning@users.noreply.github.com>
2021-01-02 22:56:53 +0800

    lesson 5#完成列表组件的编写

--- src/components/TodoList/List/index.tsx
+++ src/components/TodoList/List/index.tsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { FC, ReactElement } from 'react'
+import { ITodo } from '../typings'
+import TdItem from './Item'
 
-const TdList = () => {
+interface IProps {
+    todoList: ITodo[],
+    toggleTodo: (id: number) => void;
+    removeTodo: (id: number) => void;
+}
+
+const TdList: FC<IProps> = ({
+    todoList,
+    toggleTodo,
+    removeTodo
+}): ReactElement => {
     return (
         <div className="td-list">
+            {
+                todoList && todoList.map((todo: ITodo) => {
+                    return (
+                        <TdItem
+                            key={todo.id}
+                            todo={todo}
+                            toggleTodo={toggleTodo}
+                            removeTodo={removeTodo}
+                        />
+                    )
+                })
+            }
         </div>
     )
 }
--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -26,12 +26,20 @@ const TodoList: FC = (): ReactElement => {
     // 关于父组件传递props[属性与方法], 父组件更新, 子组件并不需要更新的情况, 函数句柄会重新生成, 不符合性能优化.
     // useCallback()的使用, 在react hooks社区讨论中, 仁者见仁, 智者见智. 但视频作者认为: useCallback只有好处, 建议使用.[即 如果子组件中方法是父组件传递过来, 都用useCallback包裹一下]
     // https://react.docschina.org/docs/hooks-reference.html#usecallback
-    const addTodo = useCallback((todo: ITodo) => {
+    const addTodo = useCallback((todo: ITodo): void => {
         // setTodoList(todoList => [...todoList, todo])
         dispatch({
             type: ACTION_TYPE.ADD_TODO,
             payload: todo
         })
+    }, [])
+
+    const removeTodo = useCallback((id: number): void => {
+     
+    }, [])
+
+    const toggleTodo = useCallback((id: number): void => {
+       
     }, [])
 
     return (
@@ -41,7 +49,11 @@ const TodoList: FC = (): ReactElement => {
                 addTodo={addTodo}
                 todoList={state.todoList}
             />
-            <TdList />
+            <TdList
+                todoList={state.todoList}
+                removeTodo={removeTodo}
+                toggleTodo={toggleTodo}
+            />
         </div>
     )
 }
